328372d7a5f0cb041fa66e5b484f8d32dffc95ef
Artem Serebryakov <avsrb@icloud.com>
2022-01-23 18:50:25 +0300

    Added switching by colors of the rainbow

--- The Light/ViewController.swift
+++ The Light/ViewController.swift
@@ -8,23 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var isLightOn: Bool = false
+    var color = 0
     override var prefersStatusBarHidden: Bool {
         return true
     }
-    
+    // MARK: - Metods
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
 
     fileprivate func updateUI() {
-        view.backgroundColor = isLightOn ? .white : .black
+        switch color {
+        case 0:
+            view.backgroundColor = .white
+        case 1:
+            view.backgroundColor = .red
+        case 2:
+            view.backgroundColor = .orange
+        case 3:
+            view.backgroundColor = .yellow
+        case 4:
+            view.backgroundColor = .green
+        case 5:
+            view.backgroundColor = .blue
+        case 6:
+            view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .black
+        }
     }
     
-    @IBAction func buttonPressed() {
-        isLightOn.toggle()
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        color += 1
+        if color > 7 {
+            color = 0
+        }
         updateUI()
     }
+    
 }
 
